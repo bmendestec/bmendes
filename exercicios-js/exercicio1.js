fb8f6ae8ee80ef3ae07b7a96c1b2105f7ab33531
Bruno Mendes <bmendestecnologia@gmail.com>
2023-10-02 20:37:30 -0300

    atividades 02.10

--- exercicios-js/exercicio1.js
+++ exercicios-js/exercicio1.js
@@ -15,3 +15,16 @@ function converteIdadeDeAnoEmDias(idade){
 
 converteIdadeDeAnoEmDias(25)
 converteIdadeDeAnoEmDias(70)
+
+
+/*Função factory desafio*/
+function criarProduto(nome, preco){
+    return {
+        nome,
+        preco,
+        desconto: preco * 0.1
+    }
+}
+
+console.log(criarProduto('Algodão', 15.40))
+console.log(criarProduto('iPad', 1199.49))
